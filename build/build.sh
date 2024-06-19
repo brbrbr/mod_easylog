da6ed3a259c28b5e808f420ab55acc75b23a607e
bram <github@brambring.nl>
2024-06-19 11:25:14 +0200

    Installer correct

--- build/build.sh
+++ build/build.sh
@@ -9,7 +9,7 @@ if [ -f "$EXTENSION_ZIP_FILENAME" ]; then rm $EXTENSION_ZIP_FILENAME; fi
 sed -i -e "s/$PATTERN/$VERSION/g" $EXTENSION_ELEMENT.xml
 sed -i -e "s/$PATTERN/$VERSION/g" 44.sh
 
-zip -r $EXTENSION_ZIP_FILENAME language/ "$EXTENSION_ELEMENT.xml"  services/ forms/ src/ script.php --quiet
+zip -r $EXTENSION_ZIP_FILENAME language/ "$EXTENSION_ELEMENT.xml"  CHANGELOG.txt  LICENSE  tmpl/ README.md services/ forms/ src/ script.php --quiet
 
 SHA512=$(sha512sum $EXTENSION_ZIP_FILENAME | awk '{print $1}')
 sed -i -e "0,/\(<sha512>\).*\(<\/sha512>\)/s//<sha512>$SHA512<\/sha512>/"  \
